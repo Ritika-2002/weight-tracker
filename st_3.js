298b0ede4d84bb2f84b5e3447c29a2a03544dea4
Ritika Grover <ritikagrover1112@gmail.com>
2022-09-21 16:07:22 +0530

    project

---
+++ st_3.js
@@ -0,0 +1,143 @@
+// SUN
+function f1() {
+    var n = document.getElementById("given_weight").value;
+    n = Number(n);
+    if (isNaN(n) || n == ' ') {
+        alert("Please insert a number");
+    } else {
+        let gmer = 27.01;
+
+        let newmass = n * gmer;
+        document.getElementById("new_weight").innerHTML = newmass;
+        document.getElementById("name_planet").innerHTML = "Sun";
+    }
+}
+//MERCURY
+function f2() {
+    var n = document.getElementById("given_weight").value;
+    n = Number(n);
+    if (isNaN(n) || n == ' ') {
+        alert("Please insert a number");
+    } else {
+        let gmer = 0.38;
+
+        let newmass = n * gmer;
+        document.getElementById("new_weight").innerHTML = newmass;
+        document.getElementById("name_planet").innerHTML = "Mercury";
+         
+    }
+}
+//VENUS
+function f3() {
+    var n = document.getElementById("given_weight").value;
+    n = Number(n);
+    if (isNaN(n) || n == ' ') {
+        alert("Please insert a number");
+    } else {
+        let gmer = 0.91;
+
+        let newmass = n * gmer;
+        document.getElementById("new_weight").innerHTML = newmass;
+        document.getElementById("name_planet").innerHTML = "Venus";
+    }
+}
+//EARTH
+function f4() {
+    var n = document.getElementById("given_weight").value;
+    n = Number(n);
+    if (isNaN(n) || n == ' ') {
+        alert("Please insert a number");
+    } else {
+        let gmer = 1;
+
+        let newmass = n * gmer;
+        document.getElementById("new_weight").innerHTML = newmass;
+        document.getElementById("name_planet").innerHTML = "Earth";
+    }
+}
+//MARS
+function f5() {
+    var n = document.getElementById("given_weight").value;
+    n = Number(n);
+    if (isNaN(n) || n == ' ') {
+        alert("Please insert a number");
+    } else {
+        let gmer = 0.38;
+
+        let newmass = n * gmer;
+        document.getElementById("new_weight").innerHTML = newmass;
+        document.getElementById("name_planet").innerHTML = "Mars";
+    }
+}
+//JUPITER
+function f6() {
+    var n = document.getElementById("given_weight").value;
+    n = Number(n);
+    if (isNaN(n) || n == ' ') {
+        alert("Please insert a number");
+    } else {
+        let gmer = 2.34;
+
+        let newmass = n * gmer;
+        document.getElementById("new_weight").innerHTML = newmass;
+        document.getElementById("name_planet").innerHTML = "Jupiter";
+    }
+}
+//SATURN
+function f7() {
+    var n = document.getElementById("given_weight").value;
+    n = Number(n);
+    if (isNaN(n) || n == ' ') {
+        alert("Please insert a number");
+    } else {
+        let gmer = 1.06;
+
+        let newmass = n * gmer;
+        document.getElementById("new_weight").innerHTML = newmass;
+        document.getElementById("name_planet").innerHTML = "Saturn";
+    }
+}
+//URANUS
+function f8() {
+    var n = document.getElementById("given_weight").value;
+    n = Number(n);
+    if (isNaN(n) || n == ' ') {
+        alert("Please insert a number");
+    } else {
+        let gmer = 0.92;
+
+        let newmass = n * gmer;
+        document.getElementById("new_weight").innerHTML = newmass;
+        document.getElementById("name_planet").innerHTML = "Uranus";
+    }
+}
+//NEPTUNE
+function f9() {
+    var n = document.getElementById("given_weight").value;
+    n = Number(n);
+    if (isNaN(n) || n == ' ') {
+        alert("Please insert a number");
+    } else {
+        let gmer = 1.19;
+
+        let newmass = n * gmer;
+        document.getElementById("new_weight").innerHTML = newmass;
+        document.getElementById("name_planet").innerHTML = "Neptune";
+    }
+}
+//PLUTO
+function f10() {
+    var n = document.getElementById("given_weight").value;
+    n = Number(n);
+    if (isNaN(n) || n == ' ') {
+        alert("Please insert a number");
+    } else {
+        let gmer = 0.06;
+
+        let newmass = n * gmer;
+        document.getElementById("new_weight").innerHTML = newmass;
+        document.getElementById("name_planet").innerHTML = "Pluto";
+    }
+}
+ 
+
